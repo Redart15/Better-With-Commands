10661d4a11068f7fe60509080984c508233caf95
Redart15 <redart15@users.noreply.github.com>
2025-11-01 20:55:47 +0100

    renamed some stuff and capsuled it to server and client only

--- src/main/java/redart15/commandly/CommandlyServer.java
+++ src/main/java/redart15/commandly/CommandlyServer.java
@@ -1,14 +1,14 @@
 package redart15.commandly;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
-import redart15.commandly.command.CommadlyCommands;
+import redart15.commandly.command.CommandlyCommands;
 
 import static redart15.commandly.CommandlyMod.LOGGER;
 
 public class CommandlyServer implements DedicatedServerModInitializer {
 	@Override
 	public void onInitializeServer() {
-		CommadlyCommands.registerServerCommands();
+		CommandlyCommands.registerServerCommands();
 		LOGGER.info("Commandly server initialized.");
 	}
 }
