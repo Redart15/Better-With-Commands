14946aa87d1fb2e9955978fad0077061f61403c8
Redart15 <redart15@users.noreply.github.com>
2025-11-01 20:55:47 +0100

    renamed some stuff and capsuled it to server and client only

--- src/main/java/redart15/commandly/CommandlyClient.java
+++ src/main/java/redart15/commandly/CommandlyClient.java
@@ -3,14 +3,14 @@ package redart15.commandly;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import redart15.commandly.command.CommadlyCommands;
+import redart15.commandly.command.CommandlyCommands;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 
 @Environment(EnvType.CLIENT)
 public class CommandlyClient implements ClientModInitializer, ClientStartEntrypoint {
 	@Override
 	public void onInitializeClient() {
-		CommadlyCommands.registerClientCommands();
+		CommandlyCommands.registerClientCommands();
 	}
 
 	@Override
--- src/main/java/redart15/commandly/command/CommandlyCommands.java
+++ src/main/java/redart15/commandly/command/CommandlyCommands.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.core.net.command.CommandManager;
 
-public class CommadlyCommands {
+public class CommandlyCommands {
 
 
 	@Environment(EnvType.CLIENT)
