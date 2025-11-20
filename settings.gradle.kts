136d72fd28e973b1f351505086dab06063774716
Redart15 <redart15@users.noreply.github.com>
2025-11-20 19:26:06 +0100

    updated build script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,19 @@
+val modName = providers.gradleProperty("mod_name")
+rootProject.name = modName.get()
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		maven {
-			name = "Jitpack"
-			url = uri("https://jitpack.io")
-		}
-		maven {
-			name = "Babric"
-			url = uri("https://maven.glass-launcher.net/babric")
-		}
-		maven {
-			name = "SignalumMavenInfrastructure"
-			url = uri("https://maven.thesignalumproject.net/infrastructure")
-		}
+		maven("https://maven.fabricmc.net/") { name = "Fabric" }
+		maven("https://jitpack.io") { name = "Jitpack" }
+		maven("https://maven.thesignalumproject.net/infrastructure") { name = "SignalumMavenInfrastructure" }
 	}
+	val foojayResolverVersion = providers.gradleProperty("foojay_resolver_version")
+	val loomVersion = providers.gradleProperty("loom_version")
+	plugins {
+		id("org.gradle.toolchains.foojay-resolver-convention").version(foojayResolverVersion.get())
+		id("fabric-loom").version(loomVersion.get())
+	}
+}
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention")
 }
