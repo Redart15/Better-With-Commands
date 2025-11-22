3c51970ebb9bf3e2f299c4dbbb03a9ef1bd87c4c
Redart15 <redart15@users.noreply.github.com>
2025-11-22 22:35:33 +0100

    adjusted and simplified the register for both ore and pickaxe register

--- src/main/java/redart15/commandly/veincapitator/PickAxeRegister.java
+++ src/main/java/redart15/commandly/veincapitator/PickAxeRegister.java
@@ -9,43 +9,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PickAxeRegister {
-	protected static final Map<Integer, ToolMaterial> toolSet = new HashMap();
+	protected static final PickAxeRegister instance = new PickAxeRegister();
+	protected static final Map<Integer, ToolMaterial> toolSet = new HashMap<>();
 
+	public static void init() {
+		// loads the class
+	}
+
+	protected PickAxeRegister() {
+	}
+
+	public static PickAxeRegister getInstance() {
+		return instance;
+	}
 
 	public static boolean containsID(int id) {
 		return toolSet.containsKey(id);
 	}
 
-	public static boolean addPickaxe(Item pickaxe) {
-		try {
-			Method method = pickaxe.getClass().getMethod("getMaterial");
-			ToolMaterial toolMaterial = (ToolMaterial) method.invoke(pickaxe);
-			toolSet.put(pickaxe.id, toolMaterial);
-			return true;
-		} catch (NoSuchMethodException e) {
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static boolean register(ItemTool pickaxe) {
+		if (pickaxe == null) {
 			return false;
 		}
+		toolSet.put(pickaxe.id, pickaxe.getMaterial());
+		return true;
 	}
 
-	public static ToolMaterial getMaterial(Item item){
-		if(item instanceof ItemTool){
+	public static boolean register(Item pickaxe, ToolMaterial material) {
+		if (pickaxe == null || material == null) {
+			return false;
+		}
+		toolSet.put(pickaxe.id, material);
+		return true;
+	}
+
+	public static ToolMaterial getMaterial(Item item) {
+		if (item instanceof ItemTool) {
 			return ((ItemTool) item).getMaterial();
 		}
 		return PickAxeRegister.getMaterial(item.id);
 	}
 
-	private static ToolMaterial getMaterial(int id){
+	private static ToolMaterial getMaterial(int id) {
 		return toolSet.getOrDefault(id, ToolMaterial.wood);
 	}
 
-	public static int getMiningLevel(Item item){
+	public static int getMiningLevel(Item item) {
 		return PickAxeRegister.getMaterial(item).getMiningLevel();
 	}
 
-	private static int getMiningLevel(int id){
+	private static int getMiningLevel(int id) {
 		return PickAxeRegister.getMaterial(id).getMiningLevel();
 	}
 
