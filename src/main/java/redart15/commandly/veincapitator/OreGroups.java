a4c2ed22fb1324a83b72c092cf50054682e7dea0
Redart15 <redart15@users.noreply.github.com>
2025-11-22 22:35:33 +0100

    adjusted and simplified the register for both ore and pickaxe register

--- src/main/java/redart15/commandly/veincapitator/OreGroups.java
+++ src/main/java/redart15/commandly/veincapitator/OreGroups.java
@@ -7,90 +7,122 @@ import net.minecraft.core.util.collection.NamespaceID;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /// Ore -> Group -> List of Group members
 public class OreGroups {
-	public static final OreGroups instance = new OreGroups();
-	public Map<String, HashSet<NamespaceID>> GROUP_TO_BLOCKS = new HashMap<>();
-	public Map<NamespaceID, String> BLOCKS_TO_GROUP = new HashMap<>();
+	protected static final OreGroups instance = new OreGroups();
+	protected static final Map<String, HashSet<NamespaceID>> GROUP_TO_BLOCKS = new HashMap<>();
+	protected static final Map<NamespaceID, String> BLOCKS_TO_GROUP = new HashMap<>();
 
-	public static void init(){}
+	public static class OreGroupBuilder {
+		private final String oreGroupName;
+
+		private OreGroupBuilder(String oreGroupName) {
+			this.oreGroupName = oreGroupName;
+		}
+
+		/**
+		 * @implNote Returns a builder to allow chain add.
+		 * */
+		public static OreGroupBuilder register(String oreGroup) {
+			return new OreGroupBuilder(oreGroup);
+		}
+
+		/**
+		 * @implNote Add ore and its variation to a lookup table.
+		 * */
+		public OreGroupBuilder addOre(Block<?> block) {
+			instance.addOreGroup(this.oreGroupName, block);
+			return this;
+		}
+	}
+
+	public static void init() {
+		// no need
+	}
+
+	public static OreGroups getInstance(){
+		return instance;
+	}
+
+	public static OreGroupBuilder register(String string){
+		return new OreGroupBuilder(string);
+	}
 
 	protected OreGroups() {
-		this.register();
+		this.load();
 	}
 
-	private void register() {
-		this.addOreGroups("coal", Blocks.ORE_COAL_BASALT);
-		this.addOreGroups("coal", Blocks.ORE_COAL_GRANITE);
-		this.addOreGroups("coal", Blocks.ORE_COAL_LIMESTONE);
-		this.addOreGroups("coal", Blocks.ORE_COAL_STONE);
-		this.addOreGroups("coal", Blocks.ORE_COAL_PERMAFROST);
+	private void load() {
+		OreGroups.register("coal")
+			.addOre(Blocks.ORE_COAL_BASALT)
+			.addOre(Blocks.ORE_COAL_GRANITE)
+			.addOre(Blocks.ORE_COAL_LIMESTONE)
+			.addOre(Blocks.ORE_COAL_STONE)
+			.addOre(Blocks.ORE_COAL_PERMAFROST);
 
-		this.addOreGroups("diamond", Blocks.ORE_DIAMOND_STONE);
-		this.addOreGroups("diamond",Blocks.ORE_DIAMOND_BASALT);
-		this.addOreGroups("diamond",Blocks.ORE_DIAMOND_GRANITE);
-		this.addOreGroups("diamond",Blocks.ORE_DIAMOND_LIMESTONE);
-		this.addOreGroups("diamond", Blocks.ORE_DIAMOND_PERMAFROST);
+		OreGroups.register("diamond")
+			.addOre(Blocks.ORE_DIAMOND_STONE)
+			.addOre(Blocks.ORE_DIAMOND_BASALT)
+			.addOre(Blocks.ORE_DIAMOND_GRANITE)
+			.addOre(Blocks.ORE_DIAMOND_LIMESTONE)
+			.addOre(Blocks.ORE_DIAMOND_PERMAFROST);
 
-		this.addOreGroups("gold",Blocks.ORE_GOLD_STONE);
-		this.addOreGroups("gold",Blocks.ORE_GOLD_BASALT);
-		this.addOreGroups("gold",Blocks.ORE_GOLD_GRANITE);
-		this.addOreGroups("gold",Blocks.ORE_GOLD_LIMESTONE);
-		this.addOreGroups("gold", Blocks.ORE_GOLD_PERMAFROST);
+		OreGroups.register("gold")
+			.addOre(Blocks.ORE_GOLD_STONE)
+			.addOre(Blocks.ORE_GOLD_BASALT)
+			.addOre(Blocks.ORE_GOLD_GRANITE)
+			.addOre(Blocks.ORE_GOLD_LIMESTONE)
+			.addOre(Blocks.ORE_GOLD_PERMAFROST);
 
-		this.addOreGroups("iron",Blocks.ORE_IRON_STONE);
-		this.addOreGroups("iron",Blocks.ORE_IRON_BASALT);
-		this.addOreGroups("iron",Blocks.ORE_IRON_GRANITE);
-		this.addOreGroups("iron",Blocks.ORE_IRON_LIMESTONE);
-		this.addOreGroups("iron",Blocks.ORE_IRON_PERMAFROST);
+		OreGroups.register("iron")
+			.addOre(Blocks.ORE_IRON_STONE)
+			.addOre(Blocks.ORE_IRON_BASALT)
+			.addOre(Blocks.ORE_IRON_GRANITE)
+			.addOre(Blocks.ORE_IRON_LIMESTONE)
+			.addOre(Blocks.ORE_IRON_PERMAFROST);
 
-		this.addOreGroups("nether_coal",Blocks.ORE_NETHERCOAL_NETHERRACK);
+		OreGroups.register("lapis")
+			.addOre(Blocks.ORE_LAPIS_STONE)
+			.addOre(Blocks.ORE_LAPIS_BASALT)
+			.addOre(Blocks.ORE_LAPIS_GRANITE)
+			.addOre(Blocks.ORE_LAPIS_LIMESTONE)
+			.addOre(Blocks.ORE_LAPIS_PERMAFROST);
 
-		this.addOreGroups("lapis",Blocks.ORE_LAPIS_STONE);
-		this.addOreGroups("lapis",Blocks.ORE_LAPIS_BASALT);
-		this.addOreGroups("lapis",Blocks.ORE_LAPIS_GRANITE);
-		this.addOreGroups("lapis",Blocks.ORE_LAPIS_LIMESTONE);
-		this.addOreGroups("lapis",Blocks.ORE_LAPIS_PERMAFROST);
+		OreGroups.register("nether_coal")
+			.addOre(Blocks.ORE_NETHERCOAL_NETHERRACK);
 
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_STONE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_BASALT);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GRANITE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_LIMESTONE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_PERMAFROST);
-
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GLOWING_STONE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GLOWING_BASALT);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GLOWING_GRANITE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GLOWING_LIMESTONE);
-		this.addOreGroups("redstone",Blocks.ORE_REDSTONE_GLOWING_PERMAFROST);
+		OreGroups.register("lapis")
+			.addOre(Blocks.ORE_REDSTONE_STONE)
+			.addOre(Blocks.ORE_REDSTONE_BASALT)
+			.addOre(Blocks.ORE_REDSTONE_GRANITE)
+			.addOre(Blocks.ORE_REDSTONE_LIMESTONE)
+			.addOre(Blocks.ORE_REDSTONE_PERMAFROST)
+			.addOre(Blocks.ORE_REDSTONE_GLOWING_STONE)
+			.addOre(Blocks.ORE_REDSTONE_GLOWING_BASALT)
+			.addOre(Blocks.ORE_REDSTONE_GLOWING_GRANITE)
+			.addOre(Blocks.ORE_REDSTONE_GLOWING_LIMESTONE)
+			.addOre(Blocks.ORE_REDSTONE_GLOWING_PERMAFROST);
 	}
 
-	public boolean addOreGroups(String groupName, Block<?> block) {
-		HashSet<NamespaceID> group_to_block_set = this.GROUP_TO_BLOCKS.computeIfAbsent(groupName, key -> new HashSet<>());
-		if(!group_to_block_set.add(block.namespaceId())) return false;
-		this.BLOCKS_TO_GROUP.put(block.namespaceId(), groupName);
+	private boolean addOreGroup(String groupName, Block<?> block) {
+		HashSet<NamespaceID> groupToBlockSet = GROUP_TO_BLOCKS.computeIfAbsent(groupName, key -> new HashSet<>());
+		if (!groupToBlockSet.add(block.namespaceId())) return false;
+		BLOCKS_TO_GROUP.put(block.namespaceId(), groupName);
 		block.withTags(OreTags.ORE);
 		return true;
 	}
 
-	public String getOreGroupName(Block<?> block){
-		return this.BLOCKS_TO_GROUP.getOrDefault(block.namespaceId(), "");
+	public String getOreGroupName(Block<?> block) {
+		return BLOCKS_TO_GROUP.getOrDefault(block.namespaceId(), "");
 	}
 
-	public HashSet<NamespaceID> getOreGroup(String name){
-		return this.GROUP_TO_BLOCKS.getOrDefault(name, new HashSet<>());
+	public Set<NamespaceID> getOreGroup(String name) {
+		return GROUP_TO_BLOCKS.getOrDefault(name, new HashSet<>());
 	}
 
-	public HashSet<NamespaceID> getOreGroupFromMember(Block<?> block){
+	public Set<NamespaceID> getOreGroupFromMember(Block<?> block) {
 		return getOreGroup(getOreGroupName(block));
-	}
-
-	public Map<String, HashSet<NamespaceID>> getGROUP_TO_BLOCKS(){
-		return  this.GROUP_TO_BLOCKS;
-	}
-
-	public Map<NamespaceID, String> getBLOCKS_TO_GROUP(){
-		return  this.BLOCKS_TO_GROUP;
 	}
 }
